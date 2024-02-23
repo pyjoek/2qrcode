ac055890bec75936e5aae2b1ece6cf7b07e12539
pyjoek <96807893+pyjoek@users.noreply.github.com>
2024-02-23 17:07:11 +0300

    Add files via upload
    another file

---
+++ newqrcoder.py
@@ -0,0 +1,19 @@
+import qrcode
+
+# Create a QR code object with a larger size and higher error correction
+qr = qrcode.QRCode(version=3, box_size=20, border=10, error_correction=qrcode.constants.ERROR_CORRECT_H)
+
+# Define the data to be encoded in the QR code
+data = "http://127.0.0.1:8000/"
+
+# Add the data to the QR code object
+qr.add_data(data)
+
+# Make the QR code
+qr.make(fit=True)
+
+# Create an image from the QR code with a black fill color and white background
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the QR code image
+img.save("qr_code.png")
